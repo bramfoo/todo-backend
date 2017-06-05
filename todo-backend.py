f0d538777d082a34abb1dc0b6f826cdac695fcbc
bramfoo <bramlohman+git@gmail.com>
2017-06-05 10:00:02 +0200

    Added POST call to root URL

--- todo-backend.py
+++ todo-backend.py
@@ -1,12 +1,16 @@
 from flask import Flask
+from flask import request
 from flask.ext.cors import CORS
 
 app = Flask(__name__)
 CORS(app, resources=r'/*', allow_headers="Content-Type")
 
-@app.route("/")
-def hello():
-    return "Poep World!"
+@app.route('/', methods=['GET', 'POST'])
+def todo():
+    if request.method == 'GET':
+        return "GET of root URL"
+    else:
+        return "POST to root URL"
 
 if __name__ == "__main__":
     app.run()
