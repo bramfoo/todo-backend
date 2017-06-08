2f4d0253b85eaca8433ba1564d0e1edc2871c954
bramfoo <bramlohman+git@gmail.com>
2017-06-09 00:03:13 +0200

    Added endpoint for single ToDos

--- todo/todoAPI.py
+++ todo/todoAPI.py
@@ -12,7 +12,7 @@ todos = {}
 logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/', methods=['GET', 'POST', 'DELETE'])
-def todo():
+def index():
     if request.method == 'GET':
         response = []
         for key in todos.keys():
@@ -24,12 +24,12 @@ def todo():
         req_data = request.get_json()
         logging.debug("POST data: " + str(req_data))
 
-        todoUID = uuid.uuid4().hex
+        todo_uuid = uuid.uuid4()
         todo = Item(req_data["title"])
         logging.debug("Created todo: " + str(todo))
 
-        todos[todoUID] = todo
-        return jsonify(toDict(todoUID))
+        todos[todo_uuid] = todo
+        return jsonify(toDict(todo_uuid))
 
     if request.method == 'DELETE':
         logging.debug("DELETE: " + str(request))
@@ -37,17 +37,22 @@ def todo():
         return ('', 204)
     else:
         logging.debug(request.method + str(request))
-        return
+        return ('', 204)
 
-@app.route("/<uuid:item_id>", methods=["GET"])
-def item(item_id):
-    if todos[item_id]:
-        return jsonify(toDict(item_id))
+@app.route('/<uuid:todo_uuid>', methods=["GET"])
+def todo(todo_uuid):
+    logging.debug("/<uuid>: " + str(request.path))
+    if request.method == 'GET':
+        if todo_uuid in todos:
+            return jsonify(toDict(todo_uuid))
+        else:
+            logging.debug("Item not found")
+            return jsonify(dict())
     else:
-        return jsonify(dict())
+        return ('', 204)
 
 def toDict(key):
     # Build URL based on the 'item' function
     # See http://flask.pocoo.org/docs/0.12/quickstart/#url-building
     item = todos[key]
-    return dict(title=item.title, completed=item.completed, url=url_for("item", item_id=key, _external=True))
+    return dict(title=item.title, completed=item.completed, url=url_for("todo", todo_uuid=key, _external=True))
