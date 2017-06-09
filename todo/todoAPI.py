a0706722874cf013b16a25c1bf9b2a244a8bcb22
bramfoo <bramlohman+git@gmail.com>
2017-06-09 23:59:29 +0200

    Moved logging statement to not break endpoint when no body provided

--- todo/todoAPI.py
+++ todo/todoAPI.py
@@ -40,12 +40,12 @@ def index():
 
 @app.route('/<uuid:todo_uuid>', methods=["GET", "PATCH", "DELETE"])
 def todo(todo_uuid):
-    logging.debug(str(request.method) + " on " + str(request.path) + " with " + str(request.get_json()))
+    logging.debug(str(request.method) + " on " + str(request.path))
     if todo_uuid in todos:
         logging.debug("Found: " + str(todos[todo_uuid]))
         if request.method == 'PATCH':
             req_data = request.get_json()
-            # See https://code-maven.com/how-to-insert-a-dictionary-in-another-dictionary-in-python
+            logging.debug("New data: " + str(request_data))
             todos[todo_uuid].__dict__.update(req_data.items())
             logging.debug("Updated: " + str(todos[todo_uuid]))
             return jsonify(toDict(todo_uuid))
